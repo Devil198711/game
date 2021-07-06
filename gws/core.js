c1626cd3c7dab20c2217cd6708a70b735332a9b2
Vitaly Demeschenko <v.demeschenko@gmail.com>
2021-07-06 13:02:34 +0300

    v0.0.2 Прототип рендера

--- gws/core.js
+++ gws/core.js
@@ -1,16 +1,56 @@
 class GWS {
     constructor () {
-
+        this.displayWidth = 1366;
+        this.displayHeight = 768;
+        this.point = 32;
+        this.pointX = Math.floor(1366 / 32);
+        this.pointY = Math.floor(768 / 32);
+        this.display;
+        this.loopPause = false;
     }
     displayInit(block, name = 'game-canvas', width = 1366, height = 768) {
         const display = document.querySelector(block);
         if (display) {
+            this.displayWidth = width;
+            this.displayHeight = height;
             const canvas = document.createElement('canvas');
             canvas.width = width;
             canvas.height = height;
             canvas.id = name;
+            canvas.ctx = canvas.getContext('2d')
             display.appendChild(canvas);
+            this.display = canvas;
         }
+    }
+    setDisplayPoint(point = 32) {
+        this.point = point;
+        this.pointX = Math.floor(this.displayWidth / this.point);
+        this.pointY = Math.floor(this.displayHeight / this.point);
+    }
+    getDisplay() {
+        this.display.point = this.point;
+        this.display.pointX = this.pointX;
+        this.display.pointY = this.pointY;
+        this.display.render = this.render;
+        this.display.setRender = this.setRender;
+        return this.display;
+    }
+    render() {
+
+    }
+    setRender(render) {
+        this.render = render;
+    }
+    loop() {
+        let context = this;
+        context.loopPause = false;
+        requestAnimationFrame(function () {
+            context.render();
+            context.display.ctx.clearRect(0, 0, context.width, context.height)
+            if (!context.loopPause) {
+                context.loop();
+            }
+        })
     }
 }
 export {GWS};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 "use strict";
 import {GWS} from '../gws/core.js';
-const gameScreen = new GWS;
-gameScreen.displayInit('#main', 'game', 640, 480)
+const core = new GWS;
+core.displayInit('#main', 'game', 1920, 1080, 64);
+core.setDisplayPoint(64);
+const display = core.getDisplay();
+// display.ctx.fillRect(display.point * 5, display.point * 6, display.point, display.point);
+core.setRender(function () {
+    for (let z = 0; z <= 3; z++) {
+        for (let y = 0; y <= core.pointY; y++) {
+            for (let x = 0; x <= core.pointX; x++) {
+                core.display.ctx.fillStyle = `#${Math.floor(Math.random() * 999)}`;
+                core.display.ctx.fillRect(this.point * x, core.point * y, core.point, core.point);
+            }
+        }
+    }
+});
+// console.log(core);
+// core.render()
+core.loop();
